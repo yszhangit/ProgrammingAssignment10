ec55303e62a51ab9dc7a57bfa15557ab72b2a4ed
Yinshu Zhang <yinshu.zhang.git@gmail.com>
2018-10-27 19:45:28 -0400

    day2

--- mpg_am.R
+++ mpg_am.R
@@ -8,17 +8,18 @@
 #MTcars$carb <- factor(mtcars$carb)
 #MTcars$vs <- factor(mtcars$vs, levels = c(0,1), labels = c("v-shaped","straight"))
 
+library(ggplot2)
+# ggplot2 has mpg 
 attach(mtcars)
-mod1 <- lm(mpg ~ am, data = mtcars)
-mod2 <- lm(mpg ~ wt + qsec + am, data = mtcars)
-summary(mod1)
+mod.sim <- lm(mpg ~ am, data = mtcars)
+mod.mul <- lm(mpg ~ wt + qsec + am, data = mtcars)
+summary(mod.sim)
 #resid(mod1)
-plot(density(resid(mod1))) 
+plot(density(resid(mod.sim))) 
 
 # A quantile normal plot - good for checking normality
-qqnorm(resid(mod1)) 
-qqline(resid(mod1))
-
+qqnorm(resid(mod.sim)) 
+qqline(resid(mod.sim))
 
 plot(am, mpg)
 abline(mod1)
@@ -29,13 +30,12 @@ summary(anova1)
 f_crit <- round(qf(.95, 2, 30),2)
 
 ## qqplot
-library(ggplot2)
 ggplot(data = mtcars, aes(x=am, y=mpg)) + 
   geom_point(aes(size=3, alpha=.4)) +
   stat_smooth(method = "lm") +
   ggtitle("simple linear regression between MPG and AM")
 
-ggplot(data = mtcars, aes(mod1$residuals)) + 
+ggplot(data = mtcars, aes(mod.sim$residuals)) + 
   geom_histogram(color='black', fill='white', aes(y=..density..) ) +
   geom_density(alpha=0.4, fill="grey") +
   ggtitle("fit model residual density and histogram")
@@ -86,14 +86,11 @@ ggplot(data = mtcars, aes(x=wt, y=mpg)) + geom_point(size =3, alpha =0.5, aes(co
 
 # comprae residual
 se <- function(x) sqrt(var(x)/length(x))
-par(mfrow=c(1,3))
-res.range <- ceiling(max(sapply(list(mod1$residuals, mod.two$residuals, mod.mul$residuals), range)))
+par(mfrow=c(1,2))
+res.range <- ceiling(max(sapply(list(mod.sim$residuals, mod.mul$residuals), range)))
 
-plot(mod1$residuals, ylim = c(res.range,-1 * res.range))
-abline(h=se(mod1$residuals) * c(1,-1))
-abline(h=0)
-plot(mod.two$residuals, ylim = res.range * c(1,-1))
-abline(h=se(mod.two$residuals) * c(1,-1))
+plot(mod.sim$residuals, ylim = res.range * c(1,-1))
+abline(h=se(mod.sim$residuals) * c(1,-1))
 abline(h=0)
 plot(mod.mul$residuals, ylim = res.range * c(1,-1))
 abline(h=se(mod.mul$residuals) * c(1,-1))
@@ -116,14 +113,14 @@ xy <- expand.grid( x = x.pred, y = y.pred)
 # z value by prediction
 #z.pred <- matrix(predict(mod2, newdata = xy), nrow =grid.lines, ncol = grid.lines)
 z.auto.pred <- matrix(
-  predict(mod2, newdata = data.frame(
+  predict(mod.mul, newdata = data.frame(
     wt = xy$x,
     qsec = xy$y,
     am=1)), 
   nrow =grid.lines, ncol = grid.lines
   )
 z.manual.pred <- matrix(
-  predict(mod2, newdata = data.frame(
+  predict(mod.mul, newdata = data.frame(
     wt = xy$x,
     qsec = xy$y,
     am=0)), 
@@ -132,19 +129,23 @@ z.manual.pred <- matrix(
 # droplines to surface
 #fitpoints <- predict(mod2)
 #plot
-scatter3D(wt,qsec,mpg, 
-          col= "blue", colkey=F, pch=20, cex=2 , 
-          phi =-10, theta = 10, 
+
+points.auto <- mtcars[mtcars$am == 0,]
+points.manual <- mtcars[mtcars$am == 1,]
+
+scatter3D(points.auto$wt, points.auto$qsec, points.auto$mpg, 
+          zlim = c(0,40),
+          col = "blue",
+          colkey=F, pch=20, cex=2, alpha = 0.5,
+          phi = -5, theta = 10, 
           bty = "g", 
           xlab ="wt", ylab="qsec",zlab="mpg",
-          surf = list(x = x.pred, y = y.pred, z=z.auto.pred, facets = NA ),
-          main = "mtcars"
+          surf = list(x = x.pred, y = y.pred, z=z.auto.pred, facets = NA , col = "blue"),
+          main = "MPG with auto(blue) and manual(red)"
 )
-scatter3D(wt,qsec,mpg, 
-          col= "blue", colkey=F, pch=20, cex=2 , 
-          phi =-10, theta = 10, 
-          bty = "g", 
-          xlab ="wt", ylab="qsec",zlab="mpg",
+scatter3D(points.manual$wt, points.manual$qsec, points.manual$mpg, 
+          col = "red", alpha = 0.5,
+          pch=20, ticktpe = "detailed", cex=2 , 
           surf = list(x = x.pred, y = y.pred, z=z.manual.pred, facets = NA , col = "red"),
-          main = "mtcars"
+          add = T
 )
