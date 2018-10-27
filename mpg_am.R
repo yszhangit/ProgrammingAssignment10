d7b76451b58f60cf59fe237007d740c0d988977b
Yinshu Zhang <yinshu.zhang.git@gmail.com>
2018-10-27 14:37:47 -0400

    day2

--- mpg_am.R
+++ mpg_am.R
@@ -8,6 +8,7 @@
 #MTcars$carb <- factor(mtcars$carb)
 #MTcars$vs <- factor(mtcars$vs, levels = c(0,1), labels = c("v-shaped","straight"))
 
+attach(mtcars)
 mod1 <- lm(mpg ~ am, data = mtcars)
 mod2 <- lm(mpg ~ wt + qsec + am, data = mtcars)
 summary(mod1)
@@ -19,7 +20,7 @@ qqnorm(resid(mod1))
 qqline(resid(mod1))
 
 
-plot(mtcars$am, mtcars$mpg)
+plot(am, mpg)
 abline(mod1)
 
 # ANOVA
@@ -83,3 +84,67 @@ ggplot(data = mtcars, aes(x=wt, y=mpg)) + geom_point(size =3, alpha =0.5, aes(co
   stat_smooth(method="lm") +
   ggtitle("linear regression between MPG and WT+AM")
 
+# comprae residual
+se <- function(x) sqrt(var(x)/length(x))
+par(mfrow=c(1,3))
+res.range <- ceiling(max(sapply(list(mod1$residuals, mod.two$residuals, mod.mul$residuals), range)))
+
+plot(mod1$residuals, ylim = c(res.range,-1 * res.range))
+abline(h=se(mod1$residuals) * c(1,-1))
+abline(h=0)
+plot(mod.two$residuals, ylim = res.range * c(1,-1))
+abline(h=se(mod.two$residuals) * c(1,-1))
+abline(h=0)
+plot(mod.mul$residuals, ylim = res.range * c(1,-1))
+abline(h=se(mod.mul$residuals) * c(1,-1))
+abline(h=0)
+
+par(mfrow=c(1,1))
+
+
+# 3D plot
+library(plot3D)
+#http://www.sthda.com/english/wiki/impressive-package-for-3d-and-4d-graph-r-software-and-data-visualization
+
+# init grid size
+grid.lines = 20
+#  fill points in between min and max value of indenpent variables
+x.pred <- seq(min(wt),max(wt), length.out = grid.lines)
+y.pred <- seq(min(qsec),max(qsec), length.out = grid.lines)
+# resize grid
+xy <- expand.grid( x = x.pred, y = y.pred)
+# z value by prediction
+#z.pred <- matrix(predict(mod2, newdata = xy), nrow =grid.lines, ncol = grid.lines)
+z.auto.pred <- matrix(
+  predict(mod2, newdata = data.frame(
+    wt = xy$x,
+    qsec = xy$y,
+    am=1)), 
+  nrow =grid.lines, ncol = grid.lines
+  )
+z.manual.pred <- matrix(
+  predict(mod2, newdata = data.frame(
+    wt = xy$x,
+    qsec = xy$y,
+    am=0)), 
+  nrow =grid.lines, ncol = grid.lines
+  )
+# droplines to surface
+#fitpoints <- predict(mod2)
+#plot
+scatter3D(wt,qsec,mpg, 
+          col= "blue", colkey=F, pch=20, cex=2 , 
+          phi =-10, theta = 10, 
+          bty = "g", 
+          xlab ="wt", ylab="qsec",zlab="mpg",
+          surf = list(x = x.pred, y = y.pred, z=z.auto.pred, facets = NA ),
+          main = "mtcars"
+)
+scatter3D(wt,qsec,mpg, 
+          col= "blue", colkey=F, pch=20, cex=2 , 
+          phi =-10, theta = 10, 
+          bty = "g", 
+          xlab ="wt", ylab="qsec",zlab="mpg",
+          surf = list(x = x.pred, y = y.pred, z=z.manual.pred, facets = NA , col = "red"),
+          main = "mtcars"
+)
